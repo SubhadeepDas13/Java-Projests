4d8bfc00f47654826e8e4ce943aa945dde241255
Subhadeep Das <dsubhadeep49@gmail.com>
2025-08-17 22:14:58 +0530

    Add files via upload

---
+++ arrey3.java
@@ -0,0 +1,27 @@
+public class arrey3 {
+       public static void main(String[] args)
+    {
+
+     int nums[][] = new int [6][7]; 
+    
+        for(int a =0;a<6;a++)
+    { 
+          for(int b=0;b<7;b++)
+        {
+            nums[a][b] = (int)(Math.random() * 10);
+        }
+       
+    }
+        for(int a =0;a<6;a++)
+
+    { 
+        for(int b=0;b<7;b++)
+        {
+            System.out.print(nums[a][b] + " "); 
+        }
+        System.out.println();
+    }
+
+   }
+
+}
